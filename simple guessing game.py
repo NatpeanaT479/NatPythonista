620d64a5a3a4b2ac1edecb1348d9d565706389f9
Natasha Anas <100114951+NatpeanaT479@users.noreply.github.com>
2022-08-17 23:51:45 +0800

    Update simple guessing game.py

--- simple guessing game.py
+++ simple guessing game.py
@@ -1,11 +1,6 @@
-from lib2to3.pgen2.token import COLON
-
 import random 
-
 num = random.randint(1,10)
-
 attempt = 0
-
 while attempt < 4:
     guess = int(input("Guess the number"))
     if guess == num:
